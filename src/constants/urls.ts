a0386ca8f4605aec737b26cade4cb561d75674a8
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-02-23 00:20:55 +0530

    added auth stuff

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,3 @@
 export const DB_URL = "";
 export const FRONTEND_URL = "http://localhost:3000";
+export const __prod__ = process.env.NODE_ENV === "production";
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import cors from "cors";
-import { DB_URL, FRONTEND_URL } from "./constants/urls";
+import { DB_URL, FRONTEND_URL, __prod__ } from "./constants/urls";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import redis from "redis";
 
 const main = async () => {
   await createConnection({
@@ -17,10 +20,29 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
   app.use(
     cors({
       origin: FRONTEND_URL,
       credentials: true,
+    })
+  );
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30 * 12 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: "thisissomerandomwhichbecomesusajibrish",
+      resave: false,
     })
   );
 
